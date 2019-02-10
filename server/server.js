bcbdfa1809e25a0c3722425b5498329b38ee8402
sukhadj <sukhadj1998@gmail.com>
2019-02-10 17:58:46 +0530

    Added POST /todos route and refractor mongoose

--- server/server.js
+++ server/server.js
@@ -1,52 +1,28 @@
-var mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp",{ useNewUrlParser: true });
+const {mongoose} = require('./db/mongoose.js');
+const {User} = require('./models/user.js');
+const {Todo} = require('./models/todo.js');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-      type: String,
-      required: true,
-      minlength: 1,
-      trim: true  // remove preceeding and following whitespaces
-    },
-    completed: {
-      type: Boolean,
-      default: false
-    },
-    completedAt: {
-      type: Number,
-      default: null
-    }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  },(e) => {
+    // console.log("Unable to save Todo");
+    res.status(400).send(e);
+  })
+
 });
 
-var newTodo = new Todo({
-  text: "GRE Test ",
-  completed: false,
-  completedAt: 123
+app.listen(3000, () => {
+  console.log("Started on port 3000");
 });
-
-// newTodo.save().then((doc) => {
-//   console.log(doc);
-// },(err) => {
-//   console.log("Unable to solve todo");
-// });
-
-var User = mongoose.model('User',{
-  email: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  }
-});
-
-var newUser = new User({
-  // email: "sukhadj1998"
-})
-
-newUser.save().then((doc) => {
-  console.log(doc);
-},(err) => {
-  console.log(`User cannot be saved : ${err.message}`);
-})
