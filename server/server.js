a409d2fcf4aba1ab665ed34726ec76e88495bc42
sukhadj <sukhadj1998@gmail.com>
2019-02-13 23:41:36 +0530

    Add GET /todos

--- server/server.js
+++ server/server.js
@@ -23,6 +23,19 @@ app.post('/todos',(req, res) => {
 
 });
 
+// change url
+app.get('/todos',(req, res) => {
+  Todo.findOne ({}).then((docs) => {
+    res.send({docs});
+  },(err) => {
+    res.status(400).send(err);
+  })
+});
+
 app.listen(3000, () => {
   console.log("Started on port 3000");
 });
+
+module.exports = {
+  app
+}
